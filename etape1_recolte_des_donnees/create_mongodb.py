e71eb3d13e3cc68bba93a6f57b25f69d071b965b
Ubuntu <ubuntu@ip-172-31-1-75.eu-west-1.compute.internal>
2024-03-17 15:25:57 +0000

    Ajout MongoExpress

--- etape1_recolte_des_donnees/create_mongodb.py
+++ etape1_recolte_des_donnees/create_mongodb.py
@@ -5,20 +5,26 @@ import json
 
 client = MongoClient(
     host="127.0.0.1",
-    port = 27017
+    port = 27017,
+    username = "admin",
+    password = "pass"
 )
 
-# Create Ecobalyse database
+
+# Create Ecobalyse databasedocke
 ecobalyse = client["ecobalyse"]
 print(client.list_database_names())
 
 # Create collections
 collections = ["countries", "materials", "products", "fabricProcess", "products_details"]
 
+
 for collection_name in collections:
     if collection_name not in ecobalyse.list_collection_names():
         ecobalyse.create_collection(collection_name)
         print(f"La collection {collection_name} a été créée.")
+    else:
+        ecobalyse[collection_name].delete_many({})
 
 # Load JSON files and insert data into collections
 for collection_name in collections:
