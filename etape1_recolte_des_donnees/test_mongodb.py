695b90f4511f60a75a8287e624437a91eabe7b7f
Ubuntu <ubuntu@ip-172-31-1-229.eu-west-1.compute.internal>
2024-03-28 11:31:15 +0000

    Add create_indexe.py script

--- etape1_recolte_des_donnees/test_mongodb.py
+++ etape1_recolte_des_donnees/test_mongodb.py
@@ -8,22 +8,22 @@ import os
 # Charger les variables d'environnement à partir du fichier .env
 load_dotenv()
 
-# client = MongoClient(
-#     host="127.0.0.1",
-#     port = 27017,
-#     username = "admin",
-#     password = "pass"
-# )
-
-uri = f"mongodb+srv://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@cluster0.mudexs5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
+uri = f"mongodb+srv://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@cluster0.mudexs5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(uri)
 
-
-# Create Ecobalyse databasedocke
+# Load Ecobalyse database
 ecobalyse = client["ecobalyse"]
-print(client.list_database_names())
 
-print(ecobalyse.list_collection_names())
-pprint(ecobalyse["impacts"].distinct("product_id"))
+# Retrieve collections list in database
+collections = ecobalyse.list_collection_names()
 
+# For each collection, print indexes
+for collection_name in collections:
+    collection = ecobalyse[collection_name]
+    indexes = collection.list_indexes()
+    
+    print(f"Indexes for collection '{collection_name}':")
+    for index in indexes:
+        print(index)
+    print()
