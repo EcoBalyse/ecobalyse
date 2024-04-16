9d8f24a4be43801c9c91576bf76de523ba1f1c57
Bertrand LECLERCQ <contact@nohackme.com>
2024-04-16 07:51:26 +0200

    Updated make_all.sh (Extraction)

--- extraction/make_all.sh
+++ extraction/make_all.sh
@@ -8,3 +8,12 @@ python3 get_json.py
 
 ### Make extraction ###
 python3 get_data.py -t all -v
+
+### Create Database MongoDB Atlas ###
+#python3 create_mongodb.py
+
+### Create Database Index ###
+#python3 create_indexes.py
+
+### Create Database Documentation ###
+#python3 create_doc_collection.py
