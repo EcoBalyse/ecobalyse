f07b67b3eccc16f67bc42791a9951df3e62c84c6
Bertrand LECLERCQ <contact@nohackme.com>
2024-04-26 12:10:52 +0200

    Modify DAG ecobalyse

--- airflow/dags/ecobalyse.py
+++ airflow/dags/ecobalyse.py
@@ -12,8 +12,8 @@ default_args = {
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
-    # 'retries': 1,
-    # 'retry_delay': timedelta(minutes=2),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2),
 }
 
 dag = DAG(
